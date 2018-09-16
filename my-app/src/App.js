234e27acf43438f14dbbb2a5ab327225d4b46015
elenalape <elena.l@hotmail.lt>
2018-09-16 10:56:41 -0400

    add all files

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './Just_blast_it.svg';
 import './App.css';
 import xesto from 'xesto-wave-npm';
+import exec from 'child_process';
 class App extends Component {
+
   render() {
     const client = xesto("41936d80bbfa46509ad4fcebf24c30f0");
+    let data;
     client.connect().then(controller => {
-      controller.on("Second", () => {
-        console.log("Help!");
+      fetch('https://e1bd4e6e.ngrok.io/template/actions/5b9e10bdbd35871663491f40')
+      .then( response => response.text())
+      .then(data => {
+      data = JSON.parse(data);
+      let obj = [];
+      data.forEach(point => {
+        obj.push(Object.keys(point)[0]);
       })
+      console.log(obj);
+
+      let upIndex = obj.indexOf('up')
+      let downIndex = obj.indexOf('left')
+      let leftIndex = obj.indexOf('right')
+      let rightIndex = obj.indexOf('down')
+      controller.on("Uno", () => {
+        let audio = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound' + String(upIndex + 1) + '.mp3');
+        audio.play();
+      });
+      controller.on("Dos", () => {
+        let audio = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound' + String(downIndex + 1) + '.mp3');
+        audio.play();
+      });
+      controller.on("Tres", () => {
+        let audio = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound' + String(leftIndex + 1) + '.mp3');
+        audio.play();
+      });
+      controller.on("Cuatro", () => {
+        let audio = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound' + String(rightIndex + 1) + '.mp3');
+        audio.play();
+      });
       controller.connect();
+      })
     })
+
+    var style = {
+      width:"500px",
+      height:"500px"
+    };
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src={logo} claassName="App-logo" alt="logo" style={style} />
       </div>
     );
   }
